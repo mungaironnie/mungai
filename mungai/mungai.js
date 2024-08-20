c128fb080c00ddb77279810a7a46ff4a574add9e
mungaironnie <162598367+mungaironnie@users.noreply.github.com>
2024-08-20 10:57:05 +0300

    Add files via upload

---
+++ mungai/mungai.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('load', function() {
+    var loadingScreen = document.querySelector('.loading-screen');
+    var websiteContent = document.getElementById('website-content');
+
+  
+    setTimeout(function() {
+        loadingScreen.style.display = 'none'; 
+        websiteContent.style.display = 'block'; 
+    }, 1500); 
+});
